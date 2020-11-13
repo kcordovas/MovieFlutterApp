e69bacc9e69726f0d38c32723329185e7ff4746e
kcordovas <kcordovas77@gmail.com>
2020-11-13 00:22:55 -0500

    Create HomePage

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// Home page, Route '/'
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // (REMOVE LATER) this only test
+      body: Text('Hello world'),
+    );
+  }
+}
