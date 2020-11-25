65cce1995d251fb7e59a8c18ff906f83ab1829c9
kcordovas <kcordovas77@gmail.com>
2020-11-25 15:56:02 -0500

    Create description film, with Text and SilverChildListDelegate

--- lib/src/pages/film_detail.dart
+++ lib/src/pages/film_detail.dart
@@ -11,6 +11,15 @@ class FilmDetail extends StatelessWidget {
       body: CustomScrollView(
         slivers: <Widget>[
           _createAppBar(film),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(
+                height: 10.0,
+              ),
+              _posterTitle(film, context),
+              _description(film),
+            ]),
+          ),
         ],
       ),
     );
@@ -35,6 +44,62 @@ class FilmDetail extends StatelessWidget {
           fadeInDuration: Duration(milliseconds: 150),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+
+  Widget _posterTitle(Film film, BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image(
+              image: NetworkImage(film.getPosterImg()),
+              height: 150.0,
+            ),
+          ),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  film.title,
+                  style: Theme.of(context).textTheme.headline5,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  film.originalTitle,
+                  style: Theme.of(context).textTheme.subtitle1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.star),
+                    Text(
+                      film.voteAverage.toString(),
+                      style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _description(Film film) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 10.0,
+        vertical: 20.0,
+      ),
+      child: Text(
+        film.overview,
+        textAlign: TextAlign.justify,
       ),
     );
   }
