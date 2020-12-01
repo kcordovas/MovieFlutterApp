7bf212e40abc05891aec4d1da2e727c67ecba770
kcordovas <kcordovas77@gmail.com>
2020-11-30 22:37:19 -0500

    Builded Suggestions Widget and Results (this is not neccesary)

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate {
+  String selection = '';
+  final films = ['Superman', 'Hard kill', 'Chick Fight', 'Peninswa'];
+
+  final recentsFilms = ['Spiderman', 'Iron Man'];
   @override
   List<Widget> buildActions(BuildContext context) {
     // The actions on our AppBar
@@ -28,12 +32,39 @@ class DataSearch extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     // Create Results to showing
-    return Container();
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: Colors.blueAccent,
+        child: Text(selection),
+      ),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // Suggestions when typing
-    return Container();
+
+    final suggestionList = (query.isEmpty)
+        ? recentsFilms
+        : films
+            .where((element) =>
+                element.toLowerCase().startsWith(query.toLowerCase()))
+            .toList();
+
+    return ListView.builder(
+      itemCount: suggestionList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(suggestionList[index]),
+          onTap: () {
+            selection = suggestionList[index];
+            showResults(context);
+          },
+        );
+      },
+    );
   }
 }
